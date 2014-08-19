86a67c9dfe7f0eeb69a23e006ecce3af691cc6a0
Aaron Klaassen <aaron@outerspacehero.com>
2014-08-18 23:46:16 -0500

    Launchable applications.

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,5 @@
 var $ = require("jquery");
+var child = require("child_process");
 
 var KEYS = {
   UP:     38,
@@ -8,8 +9,8 @@ var KEYS = {
   ENTER:  13
 };
 
-var totalMenuOptions = 4;
 var currentMenuSelection = 1;
+var GAMES;
 
 function handleKeyPress(keycode) {
   switch(keycode) {
@@ -19,24 +20,74 @@ function handleKeyPress(keycode) {
     case KEYS.DOWN:
       selectGame(1);
       break;
+    case KEYS.ENTER:
+      launchGame();
+      break;
   }
+}
+
+function launchGame() {
+  filename = GAMES[currentMenuSelection - 1].exec;
+  console.log("LAUNCHING: " + filename);
+
+  var game = child.exec(filename, function (error, stdout, stderr) {
+    if (error) {
+      console.log(error.stack);
+      console.log('Error code: '+error.code);
+      console.log('Signal received: '+error.signal);
+    }
+    console.log('Child Process STDOUT: '+stdout);
+    console.log('Child Process STDERR: '+stderr);
+  });
+
+  game.on('exit', function (code) {
+    console.log('Child process exited with exit code '+code);
+  });
 }
 
 function selectGame(direction) {
   currentMenuSelection += direction
 
   if (currentMenuSelection < 1)
-    currentMenuSelection = totalMenuOptions;
+    currentMenuSelection = GAMES.length;
 
-  if (currentMenuSelection > totalMenuOptions)
+  if (currentMenuSelection > GAMES.length)
     currentMenuSelection = 1;
 
   $("#gamelist li").removeClass("selected");
   $("#gamelist li:nth-child(" + currentMenuSelection + ")").addClass("selected");
 }
 
+function loadGames() {
+
+  GAMES = [
+            {
+              "name": "Calculator",
+              "exec": "/Applications/Calculator.app/Contents/MacOS/Calculator"
+            },
+            {
+              "name": "Calendar",
+              "exec": "/Applications/Calendar.app/Contents/MacOS/Calendar"
+            },
+            {
+              "name": "Safari",
+              "exec": "/Applications/Safari.app/Contents/MacOS/Safari"
+            }
+          ];
+
+  GAMES.forEach(function(game) {
+    var li = "<li>" + game.name + "</li>";
+    $("#gamelist").append(li);
+  });
+  $("#gamelist li:nth-child(1)").addClass("selected");
+
+}
+
 
 $(function() {
+
+  loadGames();
+
   $("body").keydown(function(e) {
     handleKeyPress(e.which);
   });
